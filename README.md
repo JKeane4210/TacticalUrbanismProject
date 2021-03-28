33a558075c13fb9dd8527935a3f04f5a7cc1a9a0
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-03-28 09:01:14 -0500

    Basic Project Setup w Filler 3D Model
    Used Three.js to load in a 3D model

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TacticalUrbanismProject
  Tactical Urbanism NW.js application for GS1030H
+
+ For creating a 3D model we are looking to have a gltf 3D model in the site that will show what the Wellness Hub in Lindsay Height is going to look like.
---
+++ src/main.js
@@ -0,0 +1,37 @@
+// import * as THREE from "/node_modules/three/build/three.module.js";
+
+let scene, camera, renderer, light;
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
+    camera.rotation.y = 45/180*Math.PI;
+    camera.position.x = 10;
+    camera.position.y = -5;
+    camera.position.z = 15;
+
+    let hlight = new THREE.AmbientLight(0x404040, 100);
+    scene.add(hlight);
+
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+    directionalLight.position.set(0, 1, 0);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+    
+    let light2 = new THREE.PointLight(0xc4c4c4, 10);
+    light2.position.set(0, 300, 500);
+    scene.add(light2);
+    
+    renderer = new THREE. WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    let loader = new THREE.GLTFLoader();
+    loader.load("./assets/scene.gltf", function(gltf) {
+        let car = gltf.scene.children[0];
+        scene.add(car);
+        renderer.render(scene, camera);
+        car.scale.set(10, 10, 10);
+    })
+}
+init();
