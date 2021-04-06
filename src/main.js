61c203f11a49187e1e97bb239e7317acdfbfa22a
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-04-06 13:02:36 -0500

    Enhanced buttons

--- src/main.js
+++ src/main.js
@@ -7,51 +7,58 @@
 
 let scene, camera, renderer, light, mesh;
 
-// creates the scene
-scene = new THREE.Scene();
-scene.background = new THREE.Color(0x80bde0);
-camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
-camera.rotation.y = 90/180*Math.PI;
-camera.position.x = 50;
-camera.position.y = 0;
-camera.position.z = 0;
-
-// creates ambient light
-let hlight = new THREE.AmbientLight(0x404040, 100);
-// scene.add(hlight);
-
-//creates a direcctional light -> shadow
-let directionalLight = new THREE.DirectionalLight(0xffffff, 100);
-directionalLight.position.set(0, 1, 0);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
-
-// adds a point light
-let light2 = new THREE.PointLight(0xc4c4c4, 10);
-light2.position.set(0, 300, 500);
-// scene.add(light2);
-
-// creates the renderer for the scene
-renderer = new THREE. WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// loads the 3D objectr from a GLTF loader class
-let loader = new THREE.GLTFLoader();
-loader.load("./assets/testMonkey2.glb", function(gltf) {
-    mesh = gltf.scene.children[0];
-    scene.add(mesh);
-    renderer.render(scene, camera);
-    mesh.scale.set(10, 10, 10);
-    mesh.position.set(0, 0, 0);
-    // console.log(mesh.position);
-    mainLoop();
-});
-
 var mainLoop = () => {
     requestAnimationFrame(mainLoop);
     renderer.render(scene, camera);
     mesh.rotation.y += 0.01;
-    // mesh.rotation.y = 45/180*Math.PI;
-    // console.log(mesh.rotation);
 }
+
+function init3D() {
+    // creates the scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x80bde0);
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
+    camera.rotation.y = 90/180*Math.PI;
+    camera.position.x = 50;
+    camera.position.y = 0;
+    camera.position.z = 0;
+
+    // creates ambient light
+    let hlight = new THREE.AmbientLight(0x404040, 100);
+    // scene.add(hlight);
+
+    //creates a direcctional light -> shadow
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+    directionalLight.position.set(0, 1, 0);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+
+    // adds a point light
+    let light2 = new THREE.PointLight(0xc4c4c4, 10);
+    light2.position.set(0, 300, 500);
+    // scene.add(light2);
+
+    // creates the renderer for the scene
+    renderer = new THREE. WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // loads the 3D objectr from a GLTF loader class
+    let loader = new THREE.GLTFLoader();
+    loader.load("./assets/testMonkey2.glb", function(gltf) {
+        mesh = gltf.scene.children[0];
+        scene.add(mesh);
+        renderer.render(scene, camera);
+        mesh.scale.set(10, 10, 10);
+        mesh.position.set(0, 0, 0);
+        // console.log(mesh.position);
+        mainLoop();
+    });
+}
+
+function initializeFields() {
+    
+}
+
+init3D();
+initializeFields();
