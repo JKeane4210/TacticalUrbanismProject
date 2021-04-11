353f8493ff86f98ea3d0361cf3be9e40aeacf002
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-04-11 11:37:53 -0500

    Cleaning up

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # TacticalUrbanismProject
  Tactical Urbanism NW.js application for GS1030H
 
+ We made a visual representation of the work being done in our servant leadership project that could be run as an application on any computer.
+
  For creating a 3D model we are looking to have a gltf 3D model in the site that will show what the Wellness Hub in Lindsay Height is going to look like.
+
+ Model Design: Jessica Oratch
+
+ Application Code: Jonathan Keane
--- src/questionPage.js
+++ src/questionPage.js
@@ -6,9 +6,11 @@ function intializePage() {
     let desired_question = questionsJSON.desired_question;
     let question = questionsJSON.questions[desired_question].question;
     let answer = questionsJSON.questions[desired_question].answer;
+    let image = questionsJSON.questions[desired_question].image;
     console.log(question);
     console.log(answer);
     document.getElementById("question").innerHTML = question;
+    document.getElementById("image").src = image;
     var answerTable = document.getElementById("answer");
     answer.forEach(paragraph => {
         var p = document.createElement("p");
